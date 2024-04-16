ccd9b33bbf17a626347741b61d7728def1654b14
Dragan Golic <golicdragan0@gmail.com>
2024-04-15 22:06:53 -0400

    soon will be back

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ RED_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_red_small.
 GREEN_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_green_small.png"))
 BLUE_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_blue_small.png"))
 # Player ship
-YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow_small.png"))
+YELLOW_SPACE_SHIP = pygame.image.load(os.path.join("assets", "pixel_ship_yellow.png"))
 
 # laser
 RED_LASER = pygame.image.load(os.path.join("assets", "pixel_laser_red.png"))
